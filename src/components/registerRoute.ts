745e2fe9f47779710b778776c54bf04bcecf2356
Gilbert Rodríguez <gilbertrdz.dev@gmail.com>
2024-12-12 01:26:27 -0500

    Wrap sections with <div> for better structure.
    Updated the HTML structure by wrapping each section in a <div> for improved organization and readability. This change ensures consistent nesting of elements and facilitates future styling and maintenance.

--- src/components/registerRoute.ts
+++ src/components/registerRoute.ts
@@ -2,6 +2,14 @@ import { loadPage } from "./loadPage.ts";
 import { getRouter } from "./getRouter.ts";
 import { getAppRoot } from "./getAppRoot.ts";
 
+import Alpine from 'alpinejs'
+// @ts-ignore
+import morph from '@alpinejs/morph'
+
+// @ts-ignore
+window.Alpine = Alpine
+Alpine.plugin(morph)
+
 const router = getRouter();
 const app = getAppRoot();
 
@@ -9,7 +17,8 @@ export const registerRoute = async (url: string, render: string) => {
   router.on(url, async () => {
     const content = await loadPage(render);
     if (app && content) {
-      app.innerHTML = content;
+      // @ts-ignore
+      Alpine.morph(app.children[0], content);
     }
   });
 };
